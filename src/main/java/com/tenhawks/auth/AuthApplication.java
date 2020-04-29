a3b0f61b88726c6cfeadee57e699fde223c6175a
mukhtiarahmed <mukhtiar.sherazi@gmail.com>
2020-04-29 07:38:55 +0500

    add data first time app run

--- src/main/java/com/tenhawks/auth/AuthApplication.java
+++ src/main/java/com/tenhawks/auth/AuthApplication.java
@@ -12,10 +12,10 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.core.Ordered;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.data.cassandra.core.CassandraTemplate;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -44,7 +44,7 @@ public class AuthApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(AuthApplication.class);
 
-	public static void main(String[] args)  throws Exception {
+	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
 	}
 
@@ -56,10 +56,6 @@ public class AuthApplication {
 
 	@Autowired
 	private UserRepository userRepository;
-
-	@Autowired
-	private CassandraTemplate cassandraTemplate;
-
 
 	@Bean
 	public AuthenticationKeyGenerator getAuthenticationKeyGenerator() {
@@ -90,57 +86,55 @@ public class AuthApplication {
 		dozerBean.setMappingFiles(mappingFiles);
 		return dozerBean;
 	}
-
+	
 	@Bean
 	InitializingBean sendDatabase() {
-		Client client = new Client();
-		client.setAccessTokenValiditySeconds(2592000);
-		client.setRefreshTokenValiditySeconds(2592000 * 30);
-		client.setClientId(UUID.fromString("cffe3990-6f0e-11e8-b750-4d8614c940ff"));
-		client.setClientSecret("$2a$12$.5qlSA.5Gjp9.TRlEflnXukLlYUz/eNRhLgFKubk6PIoL8GM7GzLu");
-		client.getScope().add("trust");
-		client.getAuthorizedGrantTypes().add("password");
-		client.getAuthorizedGrantTypes().add("refresh_token");
+
 
 		return () -> {
-			cassandraTemplate.truncate(Client.class);
-			cassandraTemplate.truncate(Role.class);
-			cassandraTemplate.truncate(User.class);
-			cassandraTemplate.truncate(AccessToken.class);
-			cassandraTemplate.truncate(Approval.class);
-			cassandraTemplate.truncate(ClientToken.class);
-			cassandraTemplate.truncate(RefreshToken.class);
-			clientRepository.save(client);
-			Client client1 = clientRepository.findByClientId(UUID.fromString("cffe3990-6f0e-11e8-b750-4d8614c940ff"));
 
-			Role role = new Role();
-			role.setId(UUIDs.timeBased());
-			role.setRoleName("ROLE_USER");
-		    roleRepository.save(role);
+			Client client = clientRepository.findByClientId(UUID.fromString("cffe3990-6f0e-11e8-b750-4d8614c940ff"));
+			if(client == null) {
+				client = new Client();
+				client.setAccessTokenValiditySeconds(2592000);
+				client.setRefreshTokenValiditySeconds(2592000 * 30);
+				client.setClientId(UUID.fromString("cffe3990-6f0e-11e8-b750-4d8614c940ff"));
+				client.setClientSecret("$2a$12$.5qlSA.5Gjp9.TRlEflnXukLlYUz/eNRhLgFKubk6PIoL8GM7GzLu");
+				client.getScope().add("trust");
+				client.getAuthorizedGrantTypes().add("password");
+				client.getAuthorizedGrantTypes().add("refresh_token");
+				clientRepository.save(client);
 
-			role = new Role();
-			role.setId(UUIDs.timeBased());
-			role.setRoleName("ROLE_ADMIN");
-		    roleRepository.save(role);
 
-			role = new Role();
-			role.setId(UUIDs.timeBased());
-			role.setRoleName("ROLE_SUPER_ADMIN");
-		    roleRepository.save(role);
+				Role role = new Role();
+				role.setId(UUIDs.timeBased());
+				role.setRoleName("ROLE_USER");
+				roleRepository.save(role);
 
-			User user = new User();
-			user.setUserId(UUIDs.timeBased());
-			user.setRoles(Arrays.asList(role.getRoleName()));
-			user.setEmailAddress("mukhtiar.ahmed@gmail.com");
-			user.setActive(true);
-			user.setFullName("Mukhtiar Ahmed");
-			user.setUserName("ahmed");
-			user.setPhoneNumber("+923100000000");
-			// password is  `secert`
-			user.setPassword("$2a$12$2kV1gT4c3XM.Gl1jHPoLYO/V7Pg.A1KnlUlZBrf5rnDXjYNLgc6N6");
-			userRepository.save(user);
-			user = userRepository.findByUserName("ahmed");
-			user.getRoles();
+				role = new Role();
+				role.setId(UUIDs.timeBased());
+				role.setRoleName("ROLE_ADMIN");
+				roleRepository.save(role);
+
+				role = new Role();
+				role.setId(UUIDs.timeBased());
+				role.setRoleName("ROLE_SUPER_ADMIN");
+				roleRepository.save(role);
+
+				User user = new User();
+				user.setUserId(UUIDs.timeBased());
+				user.setRoles(Arrays.asList(role.getRoleName()));
+				user.setEmailAddress("mukhtiar.ahmed@gmail.com");
+				user.setActive(true);
+				user.setFullName("Mukhtiar Ahmed");
+				user.setUserName("ahmed");
+				user.setPhoneNumber("+923100000000");
+				// password is  `secert`
+				user.setPassword("$2a$12$2kV1gT4c3XM.Gl1jHPoLYO/V7Pg.A1KnlUlZBrf5rnDXjYNLgc6N6");
+				userRepository.save(user);
+				user = userRepository.findByUserName("ahmed");
+				user.getRoles();
+			}
 		};
 	}
 
